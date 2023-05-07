b1c56cca67cd18ff6c210790fb9c70061a109fa0
opielapatryk <xpatrykopiela@gmail.com>
2023-05-07 17:32:56 +0200

    index

--- app/app/Http/Controllers/MyController.php
+++ app/app/Http/Controllers/MyController.php
@@ -3,62 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\People;
 
 class MyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $people = People::select("*")->get();
+        return view('people.index',['people'=>$people]);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        return view('people.create');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function read($id)
     {
-        //
+        $row = People::find($id);
+
+        if (!$row) {
+            return response()->json(['message' => 'Row not found'], 404);
+        }
+
+        return view('people.show', ['row' => $row]);
+    }    
+    public function update(Request $request, People $people)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+        ]);
+    
+        $people->update($request->all());
+    
+        return redirect()->route('users.index')
+                        ->with('success','Person updated successfully');
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function delete(People $people)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $people->delete();
+    
+        return redirect()->route('people.index')
+                        ->with('success','Person deleted successfully');
     }
 }
---
+++ app/resources/views/people/read.blade.php
@@ -0,0 +1,30 @@
+<!DOCTPE html>
+<html>
+<head>
+    <title>View People Record</title>
+</head>
+<body>
+    <table border = "1">
+    <tr>
+        <td>Id</td>
+        <td>Name</td>
+        <td>phone</td>
+        <td>street</td>
+        <td>city</td>
+        <td>country</td>
+        <td>email</td>
+    </tr>
+    @foreach ($people as $person)
+        <tr>
+        <td>{{ $person->id }}</td>
+        <td>{{ $person->name }}</td>
+        <td>{{ $person->phone }}</td>
+        <td>{{ $person->street }}</td>
+        <td>{{ $person->city }}</td>
+        <td>{{ $person->country }}</td>
+        <td>{{ $person->email }}</td>
+        </tr>
+        @endforeach
+    </table>
+</body>
+</html>
